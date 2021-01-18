af2327550fa5400b3844ad3dc6326f578852e151
uyttyu7532 <uyttyu7532@naver.com>
2021-01-18 15:34:13 +0900

    Migrate to AndroidX

--- app/src/main/java/org/appspot/apprtc/ConnectActivity.java
+++ app/src/main/java/org/appspot/apprtc/ConnectActivity.java
@@ -22,7 +22,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.KeyEvent;
